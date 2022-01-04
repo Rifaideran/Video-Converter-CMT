ca2b37750378509a10843186078ab397bd85be99
Rifaideran <97110997+Rifaideran@users.noreply.github.com>
2022-01-04 23:38:15 +0800

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,18 +1,18 @@
-START_TEXT = "Send me any file to begin."
+START_TEXT = "Kirim file apa saja untuk memulai."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/Collection_MovieTerbaik"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/RequestMovie84"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.3"   
+info_text = "This bot is Created by Pea Masamba\n\nWritten in python library TELETHON.\n\nBot by : @VideoConvertCMTbot\nSupport : @Collection_MovieTerbaik\n\nV1.3"   
 
-help_text = "Send me any video file to start compressing it or any kind of file to rename it.\n\n/compress - negligible loss compression\n/convert - change formats or extract audio of any video\n/rename - rename any file, extension not required\n/trim - cut your videos" 
+help_text = "Send me any media file to know the stuffs i can do.\n\ncompress - negligible loss compression\nconvert - change formats or extract audio of any video\nrename - rename any file, extension not required\ntrim - cut your videos" 
 
 source_text = "Deploy your own bot.\n\nMain branch - Personal use\nPublic branch - For your channel"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/Collection_MovieTerbaik"
 
 spam_notice = "This bot is hosted on heroku, and hence can run just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
@@ -23,3 +23,6 @@ JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
 JPG2 = "LOCAL/20211215_165751.jpg"
 
 JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
+
+JPG4 = "LOCAL/20211219_000258.jpg"
+
